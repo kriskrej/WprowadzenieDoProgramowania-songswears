6f378ddccdf8c61801736cd1839fc9f4d304465b
kris <kris@krej.net>
2019-05-31 13:41:30 +0200

    lab

--- SongSwears/SongSwears/Program.cs
+++ SongSwears/SongSwears/Program.cs
@@ -1,9 +1,52 @@
-﻿namespace SongSwears
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SongSwears
 {
-    internal class Program
+    class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
+            //var songAnalysis = new SongAnalysis("Kazik", "12 groszy");
+
+            var tekst = "Programowanie jest w chuj fajne!";
+            var censor = new Censor();
+            Console.WriteLine( censor.Fix(tekst) );
+        }
+    }
+
+    class Censor
+    {
+        string[] badWords;
+        public Censor()
+        {
+            var profanitiesFile = File.ReadAllText("profanities.txt");
+            profanitiesFile = profanitiesFile.Replace("*", "");
+            profanitiesFile = profanitiesFile.Replace("(", "");
+            profanitiesFile = profanitiesFile.Replace(")", "");
+            profanitiesFile = profanitiesFile.Replace("\"", "");
+            badWords = profanitiesFile.Split(',');
+        }
+
+        public string Fix(string tekst)
+        {
+            foreach (var word in badWords)
+            {
+                tekst = ReplaceBadWord(tekst, word);
+            }
+
+            return tekst;
+        }
+
+        private static string ReplaceBadWord(string tekst, string word)
+        {
+            var pattern = "\\b"+word+"\\b";
+            return Regex.Replace(tekst, pattern, "____", RegexOptions.IgnoreCase);
         }
     }
 }
---
+++ SongSwears/SongSwears/SongAnalysis.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net;
+
+namespace SongSwears
+{
+    internal class SongAnalysis
+    {
+
+        public SongAnalysis(string band, string songName)
+        {
+            var browser = new WebClient();
+            string url = "https://api.lyrics.ovh/v1/"+band+"/"+songName;
+            var json = browser.DownloadString(url);
+            var lyrics = JsonConvert.DeserializeObject<LyricsovhResponse>(json);
+            Console.Write(lyrics.lyrics);
+            Console.ReadKey();
+        }
+    }
+}
